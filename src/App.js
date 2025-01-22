e078335f017b9ec2e588f56e6af281a7efb07bb3
Shiva Tiwari <workshop@Shivas-MacBook-Pro.local>
2025-01-22 17:52:52 +0200

    version update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/NavBar/navBar.js";
 import Footer from "./components/Footer/Footer.js";
-import PrivacyPage from "./components/Privacy/PrivacyPage.js";
 import TermsPage from "./components/Terms/TermsPage.js";
 import HomePage from "./components/Home/HomePage.js";
 import PrivacyPolicy from "./components/Privacy/PrivacyPage.js";
@@ -13,11 +12,11 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={HomePage} />
           <Route path="/privacy-policy" component={PrivacyPolicy} />
           <Route path="/terms" component={TermsPage} />
-        </Switch>
+        </Routes>
         <Footer />
       </Router>
     </div>
--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,16 +1,7 @@
-import HamburgerMenu from "../HamburgerMenu/hamBurgerMenu";
 import "./index.css";
 import React from "react";
-import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const hideResponsiveNav = () => {
-    const menu = document.querySelector(".menu");
-    const links = document.querySelector(".nav-links");
-    links.classList.toggle("active");
-    menu.classList.toggle("change");
-  };
-
   return (
     <nav>
       <a href="/">
